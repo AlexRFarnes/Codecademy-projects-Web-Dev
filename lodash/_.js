a21dc58e5b33c53afe0e1b6b463496c64e6b2933
Alex <alexrfarnes@gmail.com>
2020-12-05 18:07:33 +0800

    clamp method implementation lodash project

--- lodash/_.js
+++ lodash/_.js
@@ -1,4 +1,24 @@
+const _ = {
+    // My initial idea for clamp method
+    // clamp(num, lowerBound, upperBound) {
+    //     if (num < lowerBound) {
+    //         return lowerBound;
+    //     } else if (num > upperBound) {
+    //         return upperBound;
+    //     } else {
+    //         return num;
+    //     }
+    // },
+    // Codecademy suggested implementation for clamp method
+    clamp(num, lowerBound, upperBound) {
+        const lowerClampedNumber = Math.max(num, lowerBound);
+        const clampedNumber = Math.min(lowerClampedNumber, upperBound);
+        return clampedNumber;
+    },
+    inRange() {
 
+    },
+};
 
 
 
