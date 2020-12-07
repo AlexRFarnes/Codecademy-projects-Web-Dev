2c1ce6f7b0c458ccde5fca1b4f54c91e620e0b64
sudo-alex1990 <alexrfarnes@gmail.com>
2020-12-07 10:15:07 +0800

    add string and object methods

--- lodash/_.js
+++ lodash/_.js
@@ -1,4 +1,6 @@
 const _ = {
+    /* ---------NUMBERS METHODS--------- */
+
     // My initial idea for clamp method
     // clamp(num, lowerBound, upperBound) {
     //     if (num < lowerBound) {
@@ -28,6 +30,46 @@ const _ = {
         }
         return false;
     },
+
+    /* ---------STRING METHODS--------- */
+    words(string){
+        let words = string.split(' ');
+        return words;
+
+    },
+    pad(string, length){
+        if(string.length > length) return string;
+
+        let totalPadding = length - string.length;
+        let beginPadding = Math.floor(totalPadding/2);
+        let endPadding = totalPadding - beginPadding;
+        return ' '.repeat(beginPadding) + string + ' '. repeat(endPadding);
+    },
+
+    /* ---------OBJECT METHODS--------- */
+    has(object, key){
+        //Check if the object's key is exists or not
+        return object[key] !== undefined
+    },
+    invert(object){
+        const invertedObject = {};
+        for(let key in object){
+            invertedObject[object[key]] = key;
+        }
+        return invertedObject;
+    },
+    findKey(object, predicate){
+        for(let key in object){
+            let value = object[key];
+            let predicateReturnValue = predicate(value);
+            if(predicateReturnValue === true) {
+                return key;
+            }
+        }
+        return undefined;
+    },
+    /* ---------ARRAY METHODS--------- */
+    
 };
 
 
